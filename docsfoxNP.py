4302c7a0bf6acaa02c06b4048b96078a8ed92e96
reggatur <admin@securemyfirm.com>
2025-05-25 12:04:08 -0500

    Update docsfoxNP.py
    Added Disclaimer and License text in comment.

--- docsfoxNP.py
+++ docsfoxNP.py
@@ -1,3 +1,37 @@
+'''
+Disclaimer:  This code resulted from an experiment in February, 2025, to see if 
+ChatGPT and CoPilot could completely write a working application without human 
+code modification. After 40+ human prompt modifications, surprisingly, the 
+experiment succeeded. The Python code is far from pretty, but it works.
+
+This comment is literally the only part of this application that was human-
+written.
+
+If you believe the code should be improved, by all means, fork it!
+
+MIT License
+
+Copyright (c) 2025 Wells H. Anderson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 from Npp import *
 import os
 import csv
